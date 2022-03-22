4023866f504fce656da51eff91394f792ac7277b
Maxim Bredin <mbredin@bk.ru>
2022-03-22 23:26:50 +0300

    Small fixes in admin.py

--- BookStore/catalog/admin.py
+++ BookStore/catalog/admin.py
@@ -7,6 +7,7 @@ from catalog.models import Department, Location, Author, Genre, Language, Pub_ho
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('num_dep', 'name')
 
+
 admin.site.register(Department, DepartmentAdmin)
 
 
@@ -20,6 +21,7 @@ admin.site.register(Location, LocationAdmin)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name')
 
+
 admin.site.register(Author, AuthorAdmin)
 
 admin.site.register(Genre)
@@ -31,11 +33,13 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author', 'actual_cost')
     list_filter = ('genre', 'language', 'author')
 
+
 admin.site.register(Book, BookAdmin)
 
 
 class ProviderAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'phone')
+
 
 admin.site.register(Provider, ProviderAdmin)
 
@@ -43,11 +47,13 @@ admin.site.register(Provider, ProviderAdmin)
 class AdmissionAdmin(admin.ModelAdmin):
     list_display = ('number', 'name_prov', 'date')
 
+
 admin.site.register(Admission, AdmissionAdmin)
 
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'patronymic', 'position', 'salary', 'phone')
+
 
 admin.site.register(Employee, EmployeeAdmin)
 
@@ -55,10 +61,12 @@ admin.site.register(Employee, EmployeeAdmin)
 class CheckAdmin(admin.ModelAdmin):
     list_display = ('number', 'date', 'employee', 'display_book_for_sale')
 
+
 admin.site.register(Check, CheckAdmin)
 
 
 class Book_for_saleAdmin(admin.ModelAdmin):
     list_display = ('barcode', 'admission', 'book', 'cost')
+
 
 admin.site.register(Book_for_sale, Book_for_saleAdmin)
