729f3c1317477b39a1a69adc05366d4c5de75eaf
Maxim Bredin <mbredin@bk.ru>
2022-03-29 20:57:24 +0300

    Add genres/, about/ and recommendations/ in urls.py

--- BookStore/catalog/urls.py
+++ BookStore/catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from catalog import views
 
-
+app_name = 'catalog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
+    path('', views.HomePageView.as_view(), name="home"),
+    path('genres/', views.GenresView.as_view(), name="genres"),
+    path('about/', views.about, name="about"),
+    path('recommendations/', views.recommendations, name="recommendations"),
 ]
