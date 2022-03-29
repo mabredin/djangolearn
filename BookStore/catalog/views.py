07165b45aea03e6e89d5c27c4572baf1e67d84ce
Maxim Bredin <mbredin@bk.ru>
2022-03-29 20:55:18 +0300

    Add GenreView(), about(), recommendations() in views

--- BookStore/catalog/views.py
+++ BookStore/catalog/views.py
@@ -1,19 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.urls import reverse
 from django.views import generic
-from .models import Book, BookInstance
+from .models import Book, BookInstance, Genre
 
 
-class IndexView(generic.ListView):
+class HomePageView(generic.ListView):
     template_name = 'catalog/index.html'
     context_object_name = 'list_books'
     paginate_by = 5
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
         return Book.objects.all()
 
     # def get(self, request, *args, **kwargs):
@@ -21,3 +19,19 @@ class IndexView(generic.ListView):
     #     # popular_books = BookInstance.check__set.filter()
     #     context = self.get_context_data()
     #     return self.render_to_response(context)
+
+
+class GenresView(generic.ListView):
+    template_name = 'catalog/genres.html'
+    context_object_name = 'list_genres'
+
+    def get_queryset(self):
+        return Genre.objects.all()
+
+
+def about(request):
+    return render(request, 'not_ready.html')
+
+
+def recommendations(request):
+    return render(request, 'not_ready.html')
